dda8c6c85b127265f47d9ff3327eda5097342231
A. Taha Baki <a.taha.baki@outlook.com>
2021-02-25 12:00:50 +0300

    :ambulance: fixed critical issue, really forgot to write getter? :poop:

--- app/src/main/java/dev/atahabaki/shamrocktoolbox/HomeActivity.kt
+++ app/src/main/java/dev/atahabaki/shamrocktoolbox/HomeActivity.kt
@@ -9,7 +9,7 @@ import java.lang.Exception
 class HomeActivity : AppCompatActivity() {
 
     private var _binding: ActivityHomeBinding? = null
-    private val binding = _binding!!
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
