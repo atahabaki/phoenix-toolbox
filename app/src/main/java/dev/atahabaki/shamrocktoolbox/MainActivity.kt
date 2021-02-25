ed03a032c28e4cf6509bdeee6fa4d0d4a309d28c
A. Taha Baki <a.taha.baki@outlook.com>
2021-02-25 06:11:15 +0300

    :tada: init...

---
+++ app/src/main/java/dev/atahabaki/shamrocktoolbox/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.atahabaki.shamrocktoolbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
