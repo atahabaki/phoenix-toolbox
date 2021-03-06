28c126bad144b1b49c40a741ddf91bece464f31e
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-06 14:57:41 +0300

    :sparkles: Quick Actions fragment kotlin file

---
+++ app/src/main/java/dev/atahabaki/shamrocktoolbox/ui/QuickActionsFragment.kt
@@ -0,0 +1,7 @@
+package dev.atahabaki.shamrocktoolbox.ui
+
+import androidx.fragment.app.Fragment
+import dev.atahabaki.shamrocktoolbox.R
+
+class QuickActionsFragment : Fragment(R.layout.fragment_quick_actions) {
+}
