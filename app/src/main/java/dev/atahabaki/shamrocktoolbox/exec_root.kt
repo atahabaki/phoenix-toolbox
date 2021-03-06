21740eb5c4c537ea281f0c9bfc6d42ef95f066f8
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-06 18:50:29 +0300

    :sparkles: execute normally...

--- app/src/main/java/dev/atahabaki/shamrocktoolbox/exec_root.kt
+++ app/src/main/java/dev/atahabaki/shamrocktoolbox/exec_root.kt
@@ -10,3 +10,12 @@ fun execRoot(command: String,tag: String) {
         Log.d(tag,"Exception thrown... ${e.message}")
     }
 }
+
+fun exec(command: String,tag: String) {
+    try {
+        val p = java.lang.Runtime.getRuntime().exec(command)
+        p.waitFor()
+    } catch (e: Exception) {
+        Log.d(tag,"Exception thrown... ${e.message}")
+    }
+}
