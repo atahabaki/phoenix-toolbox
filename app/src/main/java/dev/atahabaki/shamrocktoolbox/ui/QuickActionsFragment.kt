580de0407bbf1dc9b525fc50de968b5bb93eca43
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-06 15:01:21 +0300

    :beers: viewBinding for Quick Actions fragment...

--- app/src/main/java/dev/atahabaki/shamrocktoolbox/ui/QuickActionsFragment.kt
+++ app/src/main/java/dev/atahabaki/shamrocktoolbox/ui/QuickActionsFragment.kt
@@ -1,7 +1,24 @@
 package dev.atahabaki.shamrocktoolbox.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dev.atahabaki.shamrocktoolbox.R
+import dev.atahabaki.shamrocktoolbox.databinding.FragmentQuickActionsBinding
 
 class QuickActionsFragment : Fragment(R.layout.fragment_quick_actions) {
+    private var _binding: FragmentQuickActionsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       _binding = FragmentQuickActionsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
 }
