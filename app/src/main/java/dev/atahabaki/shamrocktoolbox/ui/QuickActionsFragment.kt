9a6bacb8b719b8de43b223d0484d74a7abc5ea80
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-06 17:12:47 +0300

    :art: reformatting code...

--- app/src/main/java/dev/atahabaki/shamrocktoolbox/ui/QuickActionsFragment.kt
+++ app/src/main/java/dev/atahabaki/shamrocktoolbox/ui/QuickActionsFragment.kt
@@ -11,18 +11,17 @@ import dev.atahabaki.shamrocktoolbox.databinding.FragmentQuickActionsBinding
 import dev.atahabaki.shamrocktoolbox.execRoot
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.lang.Exception
 
 class QuickActionsFragment : Fragment(R.layout.fragment_quick_actions) {
     private var _binding: FragmentQuickActionsBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-       _binding = FragmentQuickActionsBinding.inflate(inflater, container, false)
+        _binding = FragmentQuickActionsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -35,19 +34,18 @@ class QuickActionsFragment : Fragment(R.layout.fragment_quick_actions) {
 
     fun toggleGcam() {
         val prop = "persist.camera.HAL3.enabled"
-       try {
-           val p = java.lang.Runtime.getRuntime().exec("getprop $prop")
-           p.waitFor()
-           val stdOut = BufferedReader(InputStreamReader(p.inputStream))
-           val line = stdOut.readLine()
-           if (line.trim() == "1") {
-               execRoot("setprop $prop 0", "${activity?.packageName}.setProp")
-           }
-           else {
-               execRoot("setprop $prop 1", "${activity?.packageName}.setProp")
-           }
-       } catch (e: Exception) {
-           Log.d("${activity?.packageName}.toggleGcam","${e.message}")
-       }
+        try {
+            val p = java.lang.Runtime.getRuntime().exec("getprop $prop")
+            p.waitFor()
+            val stdOut = BufferedReader(InputStreamReader(p.inputStream))
+            val line = stdOut.readLine()
+            if (line.trim() == "1") {
+                execRoot("setprop $prop 0", "${activity?.packageName}.setProp")
+            } else {
+                execRoot("setprop $prop 1", "${activity?.packageName}.setProp")
+            }
+        } catch (e: Exception) {
+            Log.d("${activity?.packageName}.toggleGcam", "${e.message}")
+        }
     }
 }
