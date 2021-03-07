d33e0c9ca8a0e440f0577807d9c8d85ddb81029d
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-07 09:46:39 +0300

    :truck: notify funtion moved here...

--- app/src/main/java/dev/atahabaki/shamrocktoolbox/views/HomeActivity.kt
+++ app/src/main/java/dev/atahabaki/shamrocktoolbox/views/HomeActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import androidx.activity.viewModels
+import androidx.annotation.StringRes
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.snackbar.Snackbar
 import dev.atahabaki.shamrocktoolbox.R
 import dev.atahabaki.shamrocktoolbox.databinding.ActivityHomeBinding
+import dev.atahabaki.shamrocktoolbox.exec
 import dev.atahabaki.shamrocktoolbox.viewmodels.ToggleGcamViewModel
 
 class HomeActivity : AppCompatActivity() {
@@ -28,6 +31,14 @@ class HomeActivity : AppCompatActivity() {
                 add<QuickActionsFragment>(R.id.main_fragment_container)
             }
         }
+    }
+
+
+    fun notify(@StringRes resId: Int, @StringRes actionId: Int) {
+        val contextView = binding.root
+        Snackbar.make(binding.root,resId, Snackbar.LENGTH_SHORT).setAction(actionId) {
+            exec("reboot", "${activity?.packageName}.notify.reboot")
+        }.setAnchorView(R.id.main_bottom_appbar).show()
     }
 
     private fun initBottomNav() {
