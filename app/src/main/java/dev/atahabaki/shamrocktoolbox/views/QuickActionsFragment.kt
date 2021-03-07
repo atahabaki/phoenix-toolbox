c38a4a2e93f61dc8b45a496a48d32f9da2d8542f
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-07 09:44:27 +0300

    :sparkles: basic view model connection on the fragment

--- app/src/main/java/dev/atahabaki/shamrocktoolbox/views/QuickActionsFragment.kt
+++ app/src/main/java/dev/atahabaki/shamrocktoolbox/views/QuickActionsFragment.kt
@@ -7,17 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.google.android.material.snackbar.Snackbar
 import dev.atahabaki.shamrocktoolbox.R
 import dev.atahabaki.shamrocktoolbox.databinding.FragmentQuickActionsBinding
 import dev.atahabaki.shamrocktoolbox.exec
 import dev.atahabaki.shamrocktoolbox.execRoot
+import dev.atahabaki.shamrocktoolbox.viewmodels.ToggleGcamViewModel
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 class QuickActionsFragment : Fragment(R.layout.fragment_quick_actions) {
     private var _binding: FragmentQuickActionsBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: ToggleGcamViewModel by activityViewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater,
