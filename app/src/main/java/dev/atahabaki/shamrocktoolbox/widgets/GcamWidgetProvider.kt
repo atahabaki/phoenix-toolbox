4c277072f505f8b02148fcfa05e39c8d1ecb992e
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-12 21:57:47 +0300

    :package: get gcam status...

--- app/src/main/java/dev/atahabaki/shamrocktoolbox/widgets/GcamWidgetProvider.kt
+++ app/src/main/java/dev/atahabaki/shamrocktoolbox/widgets/GcamWidgetProvider.kt
@@ -5,8 +5,11 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.widget.RemoteViews
 import dev.atahabaki.shamrocktoolbox.R
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class GcamWidgetProvider : AppWidgetProvider() {
     override fun onUpdate(
@@ -24,5 +27,18 @@ class GcamWidgetProvider : AppWidgetProvider() {
             ).apply {
             }
         }
+    }
+
+    private fun getGcamStatus(context: Context?): Boolean {
+        try {
+            val p = java.lang.Runtime.getRuntime().exec("getprop $gcamProp")
+            p.waitFor()
+            val stdOut = BufferedReader(InputStreamReader(p.inputStream))
+            val line = stdOut.readLine().trim()
+            return line == "1"
+        } catch (e: Exception) {
+            Log.d("${context?.packageName}.toggleGcam", "${e.message}")
+        }
+        return false
     }
 }
