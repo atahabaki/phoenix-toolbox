aad6358bfcc9a0768dff48b2d62b22bdef952422
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-12 21:59:24 +0300

    :package: disable/enable gcam...

--- app/src/main/java/dev/atahabaki/shamrocktoolbox/widgets/GcamWidgetProvider.kt
+++ app/src/main/java/dev/atahabaki/shamrocktoolbox/widgets/GcamWidgetProvider.kt
@@ -8,10 +8,14 @@ import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
 import dev.atahabaki.shamrocktoolbox.R
+import dev.atahabaki.shamrocktoolbox.execRoot
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 class GcamWidgetProvider : AppWidgetProvider() {
+
+    private val gcamProp = "persist.camera.HAL3.enabled"
+
     override fun onUpdate(
         context: Context?,
         appWidgetManager: AppWidgetManager?,
@@ -40,5 +44,13 @@ class GcamWidgetProvider : AppWidgetProvider() {
             Log.d("${context?.packageName}.toggleGcam", "${e.message}")
         }
         return false
+    }
+
+    private fun disableGcam(context: Context?) {
+        execRoot("setprop $gcamProp 0", "${context?.packageName}.setProp")
+    }
+
+    private fun enableGcam(context: Context?) {
+        execRoot("setprop $gcamProp 1", "${context?.packageName}.setProp")
     }
 }
