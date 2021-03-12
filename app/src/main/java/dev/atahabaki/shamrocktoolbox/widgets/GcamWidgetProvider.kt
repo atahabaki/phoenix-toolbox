8b4363706df23589e751e1f7cb58a8eaf511a634
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-12 21:06:54 +0300

    :package: overriding onUpdate...

--- app/src/main/java/dev/atahabaki/shamrocktoolbox/widgets/GcamWidgetProvider.kt
+++ app/src/main/java/dev/atahabaki/shamrocktoolbox/widgets/GcamWidgetProvider.kt
@@ -1,6 +1,15 @@
 package dev.atahabaki.shamrocktoolbox.widgets
 
+import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.Context
 
 class GcamWidgetProvider : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+    }
 }
