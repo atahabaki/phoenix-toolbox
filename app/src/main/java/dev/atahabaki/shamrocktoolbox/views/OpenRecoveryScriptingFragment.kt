26aa6cfeec53836697c8fc2383065e522c07f3c8
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-26 08:58:23 +0300

    showing fab on OpenRecoveryScripting...

--- app/src/main/java/dev/atahabaki/shamrocktoolbox/views/OpenRecoveryScriptingFragment.kt
+++ app/src/main/java/dev/atahabaki/shamrocktoolbox/views/OpenRecoveryScriptingFragment.kt
@@ -5,15 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import dev.atahabaki.shamrocktoolbox.R
 import dev.atahabaki.shamrocktoolbox.adapters.RecoveryCommandsAdapter
 import dev.atahabaki.shamrocktoolbox.databinding.FragmentOpenRecoveryBinding
 import dev.atahabaki.shamrocktoolbox.models.Command
+import dev.atahabaki.shamrocktoolbox.viewmodels.FabStateViewModel
 
 class OpenRecoveryScriptingFragment: Fragment(R.layout.fragment_open_recovery) {
     private var _binding: FragmentOpenRecoveryBinding? = null
     private val binding get() = _binding!!
+
+    private val fabViewModel: FabStateViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,10 +30,8 @@ class OpenRecoveryScriptingFragment: Fragment(R.layout.fragment_open_recovery) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val adapter = RecoveryCommandsAdapter(listOf(
-            Command("install", listOf("/sdcard/flash.zip")),
-            Command("wipe", listOf("cache"))
-        ))
+        val adapter = RecoveryCommandsAdapter(listOf()) //Read from /cache at startup but for now leave it like this...
+        fabViewModel.setVisibility(true)
         binding.openRecoveryRecycler.layoutManager = LinearLayoutManager(activity)
         binding.openRecoveryRecycler.adapter = adapter
     }
