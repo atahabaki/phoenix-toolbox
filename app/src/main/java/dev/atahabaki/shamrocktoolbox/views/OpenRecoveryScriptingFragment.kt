6a20a8d592467f9d48ca3e724223d34ea38acdec
A. Taha Baki <a.taha.baki@outlook.com>
2021-04-04 19:21:50 +0300

    ✨ loadCommands at startup...

--- app/src/main/java/dev/atahabaki/shamrocktoolbox/views/OpenRecoveryScriptingFragment.kt
+++ app/src/main/java/dev/atahabaki/shamrocktoolbox/views/OpenRecoveryScriptingFragment.kt
@@ -13,9 +13,14 @@ import androidx.recyclerview.widget.RecyclerView
 import dev.atahabaki.shamrocktoolbox.R
 import dev.atahabaki.shamrocktoolbox.adapters.RecoveryCommandsAdapter
 import dev.atahabaki.shamrocktoolbox.databinding.FragmentOpenRecoveryBinding
+import dev.atahabaki.shamrocktoolbox.execRoot
+import dev.atahabaki.shamrocktoolbox.models.Command
+import dev.atahabaki.shamrocktoolbox.needsPatch
 import dev.atahabaki.shamrocktoolbox.viewmodels.FabStateViewModel
 import dev.atahabaki.shamrocktoolbox.viewmodels.RecoveryCommandViewModel
 import dev.atahabaki.shamrocktoolbox.viewmodels.RecoveryMenuStateViewModel
+import java.io.FileReader
+import java.lang.Exception
 
 class OpenRecoveryScriptingFragment: Fragment(R.layout.fragment_open_recovery) {
     private var _binding: FragmentOpenRecoveryBinding? = null
@@ -36,7 +41,10 @@ class OpenRecoveryScriptingFragment: Fragment(R.layout.fragment_open_recovery) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val adapter = RecoveryCommandsAdapter(listOf()) //Read from /cache at startup but for now leave it like this...
+        val commands = loadCommands()
+        val adapter = RecoveryCommandsAdapter(listOf())
+        recViewModel.setCommands(commands)
+        adapter.setCommands(commands)
         fabViewModel.setVisibility(true)
         binding.openRecoveryRecycler.layoutManager = LinearLayoutManager(activity)
         binding.openRecoveryRecycler.adapter = adapter
@@ -63,6 +71,37 @@ class OpenRecoveryScriptingFragment: Fragment(R.layout.fragment_open_recovery) {
                 adapter.notifyDataSetChanged()
             })
         }
+    }
+
+    private fun loadCommands(): MutableList<Command> {
+        val from = "/cache/recovery/command"
+        val to = "${activity?.cacheDir?.absolutePath}"
+        execRoot("su -c '[ -e $from ] && cp $from $to/command'", "${activity?.packageName}.applyCommands")
+        val commands = mutableListOf<Command>()
+        try {
+            val reader = FileReader("$to/command")
+            reader.forEachLine {
+                val line = it.trim()
+                if (line != "boot-recovery" && line.startsWith("--update-package=")) {
+                    val param = line.substring(17)
+                    commands.add(Command(command = "install", parameters = listOf(param)))
+                }
+                else if (line != "boot-recovery"){
+                    val seperated = line.split(" ")
+                    if (seperated.size > 1) {
+                        val params = seperated.subList(1,seperated.size-1)
+                        commands.add(Command(seperated[0], params))
+                    }
+                    else {
+                        commands.add(Command(seperated[0], listOf()))
+                    }
+                }
+            }
+        }
+        catch (e: Exception) {
+
+        }
+        return commands
     }
 
     override fun onDestroyView() {
