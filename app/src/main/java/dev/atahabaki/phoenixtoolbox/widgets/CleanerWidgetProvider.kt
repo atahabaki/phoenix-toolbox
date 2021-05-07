13011f7a9cc0bb773c5caf783ccaddc078017be6
A. Taha Baki <a.taha.baki@outlook.com>
2021-05-07 09:56:45 +0300

    🎨 added Request Code to seperate from others, fixed wrong provider in selfPendingIntent, and etc... (fixes #60)

--- app/src/main/java/dev/atahabaki/phoenixtoolbox/widgets/CleanerWidgetProvider.kt
+++ app/src/main/java/dev/atahabaki/phoenixtoolbox/widgets/CleanerWidgetProvider.kt
@@ -12,17 +12,21 @@ import dev.atahabaki.phoenixtoolbox.deleteCache
 
 class CleanerWidgetProvider: AppWidgetProvider() {
     companion object {
+        const val REFRESH_ACTION = "android.appwidget.action.APPWIDGET_UPDATE"
         const val ACTION_CLEAN = "dev.atahabaki.phoenixtoolbox.appwidget.CLEAN"
+        const val REQUEST_CODE = 454923
     }
 
     private fun updateWidgets(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
         appWidgetIds?.forEach {
-            val views: RemoteViews = RemoteViews(context?.packageName, R.layout.widget_cleaner)
-                    .apply {
-                setOnClickPendingIntent(R.id.cleaner_widget_clean,
-                        selfPendingIntent(context, ACTION_CLEAN))
+            if (appWidgetManager?.getAppWidgetInfo(it)?.label == context?.getString(R.string.clear_cache)) {
+                val views: RemoteViews = RemoteViews(context?.packageName, R.layout.widget_cleaner)
+                        .apply {
+                            setOnClickPendingIntent(R.id.cleaner_widget_clean,
+                                    selfPendingIntent(context, ACTION_CLEAN))
+                        }
+                appWidgetManager?.updateAppWidget(it, views)
             }
-            appWidgetManager?.updateAppWidget(it, views)
         }
     }
 
@@ -31,9 +35,9 @@ class CleanerWidgetProvider: AppWidgetProvider() {
     }
 
     private fun selfPendingIntent(context: Context?, action: String) : PendingIntent {
-        val intent = Intent(context, GcamWidgetProvider::class.java)
+        val intent: Intent = Intent(context, CleanerWidgetProvider::class.java)
         intent.action = action
-        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        return PendingIntent.getBroadcast(context, REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT)
     }
 
     override fun onReceive(context: Context?, intent: Intent?) {
@@ -44,9 +48,10 @@ class CleanerWidgetProvider: AppWidgetProvider() {
         }
         super.onReceive(context, intent)
         if (intent?.action.equals(ACTION_CLEAN)) {
-            deleteCache(context!!.packageName)
+            deleteCache(context?.packageName!!)
+            update()
         }
-        else {
+        else if (intent?.action.equals(REFRESH_ACTION)) {
             update()
         }
     }
