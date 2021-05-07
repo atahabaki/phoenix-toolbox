d6900f4f81cd480e6d6b074bacbae7dfd182ca1f
A. Taha Baki <a.taha.baki@outlook.com>
2021-05-07 08:32:34 +0300

    ✨ added CleanerWidgetProvider (talks with itself :D )...

---
+++ app/src/main/java/dev/atahabaki/phoenixtoolbox/widgets/CleanerWidgetProvider.kt
@@ -0,0 +1,53 @@
+package dev.atahabaki.phoenixtoolbox.widgets
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import dev.atahabaki.phoenixtoolbox.R
+import dev.atahabaki.phoenixtoolbox.deleteCache
+
+class CleanerWidgetProvider: AppWidgetProvider() {
+    companion object {
+        const val ACTION_CLEAN = "dev.atahabaki.phoenixtoolbox.appwidget.CLEAN"
+    }
+
+    private fun updateWidgets(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
+        appWidgetIds?.forEach {
+            val views: RemoteViews = RemoteViews(context?.packageName, R.layout.widget_cleaner)
+                    .apply {
+                setOnClickPendingIntent(R.id.cleaner_widget_clean,
+                        selfPendingIntent(context, ACTION_CLEAN))
+            }
+            appWidgetManager?.updateAppWidget(it, views)
+        }
+    }
+
+    override fun onUpdate(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
+        updateWidgets(context,appWidgetManager,appWidgetIds)
+    }
+
+    private fun selfPendingIntent(context: Context?, action: String) : PendingIntent {
+        val intent = Intent(context, GcamWidgetProvider::class.java)
+        intent.action = action
+        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        fun update() {
+            val appWidgetManager: AppWidgetManager = AppWidgetManager.getInstance(context!!)
+            val appWidgetIds = appWidgetManager.getAppWidgetIds(ComponentName(context, GcamWidgetProvider::class.java))
+            updateWidgets(context, appWidgetManager, appWidgetIds)
+        }
+        super.onReceive(context, intent)
+        if (intent?.action.equals(ACTION_CLEAN)) {
+            deleteCache(context!!.packageName)
+        }
+        else {
+            update()
+        }
+    }
+}
